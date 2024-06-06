2f5d63520717bf54e932c114dbdc6b35b5a0a761
gitphnav24 <163900079+gitphnav24@users.noreply.github.com>
2024-06-06 18:13:25 +0200

    Add files via upload
    F1Breadcrumbs in F1AllTeams.js ALTERED

--- src/Components/F1AllDrivers.js
+++ src/Components/F1AllDrivers.js
@@ -35,7 +35,7 @@ const F1AllDrivers = (props) => {
     useEffect(() => {
         setIsLoading(true);
         getAllDrivers();
-    }, [year]);
+    }, []);
 
     if (isLoading) {
         return <Loader />
@@ -69,6 +69,7 @@ const F1AllDrivers = (props) => {
         <div className="header">
             <F1Breadcrumbs items={items} />
             <input
+                name="crumbs"
                 type="text"
                 placeholder="Search by driver name"
                 value={searchTerm}
